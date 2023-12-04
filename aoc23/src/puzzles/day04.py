623b99ba902c287c55f738d4fb55cb3a59c47d6b
Alex Mabry <alex.mabry@adtran.com>
2023-12-04 11:07:56 -0600

    fun: day 4

--- aoc23/src/puzzles/day04.py
+++ aoc23/src/puzzles/day04.py
@@ -1,15 +1,30 @@
+import re
+
 from ..utils import parse_data
+
+CARD = r"Card\s+\d+: ([\d\s]+)\s+\|\s+([\d\s]+)"
+
+
+def get_winning_numbers(data):
+    input_data = parse_data(data)
+    cards = [match.groups() for row in input_data for match in re.finditer(CARD, row)]
+    numbers = [[{int(n) for n in side.split()} for side in sides] for sides in cards]
+    return [len(winner & player) for [winner, player] in numbers]
 
 
 def solve_part1(data: str):
-    input_data = parse_data(data)
-    print(input_data)
-
-    return None
+    winning_numbers = get_winning_numbers(data)
+    points = [2 ** (count - 1) for count in winning_numbers if count]
+    return sum(points)
 
 
 def solve_part2(data: str):
-    input_data = parse_data(data)
-    print(input_data)
+    winning_numbers = get_winning_numbers(data)
+    counts = {n + 1: count for n, count in enumerate(winning_numbers)}
+    copies = {n + 1: 1 for n in range(len(counts))}
 
-    return None
+    for card in range(1, len(counts) + 1):
+        for winner in range(card + 1, card + 1 + counts[card]):
+            copies[winner] += copies[card]
+
+    return sum(copies.values())
