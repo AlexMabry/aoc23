b1aa2617cdca95d0ea9bd752444561fc406ea0b7
Alex Mabry <alex.mabry@adtran.com>
2023-12-06 08:55:22 -0600

    fun: day 6 part 1

--- aoc23/src/puzzles/day06.py
+++ aoc23/src/puzzles/day06.py
@@ -1,11 +1,16 @@
+import re
+
 from ..utils import parse_data
 
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    races = [map(int, r) for r in zip(*(re.findall(r"(\d+)", l) for l in input_data))]
+    result = 1
+    for time, dist in races:
+        result *= sum(1 for tick in range(1, time) if tick * (time - tick) > dist)
 
-    return None
+    return result
 
 
 def solve_part2(data: str):
