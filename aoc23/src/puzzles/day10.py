206292c8e526f47589968aa8465407e785386910
Alex Mabry <alex.mabry@adtran.com>
2023-12-11 12:00:51 -0600

    fun: day 10 cleanup

--- aoc23/src/puzzles/day10.py
+++ aoc23/src/puzzles/day10.py
@@ -17,8 +17,7 @@ TURN = {
 }
 
 
-def solve_part1(data: str):
-    input_data = parse_data(data)
+def find_path(input_data):
     board = {
         Point(x, y): char
         for y, row in enumerate(input_data)
@@ -32,68 +31,50 @@ def solve_part1(data: str):
     )
 
     path = [start]
+    turns = [facing]
     while step not in path:
         path.append(step)
         facing = TURN[board[step]][facing]
+        turns.append(facing)
         step = step.neighbor(facing)
+
+    return path, turns
+
+
+def solve_part1(data: str):
+    input_data = parse_data(data)
+    path, turns = find_path(input_data)
 
     return len(path) // 2
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
-    board = {
-        Point(x, y): char
-        for y, row in enumerate(input_data)
-        for x, char in enumerate(row)
-    }
+    path, turns = find_path(input_data)
 
-    start = next(k for k, v in board.items() if v == "S")
-    facing, step = next(
-        (direction, point)
-        for direction, point in start.cardinal_neighbors().items()
-        if point in board and board[point] in ALLOWED[direction]
+    # expand the board by a factor of 2
+    H, W = len(input_data) * 2, len(input_data[0]) * 2
+    expanded = {Point(x, y) for y in range(H) for x in range(W)}
+
+    # remove the path and the turns
+    exp_path = set().union(
+        {
+            np
+            for point, facing in zip(path, turns)
+            for np in (point * 2, (point * 2).neighbor(facing))
+        }
     )
+    expanded -= exp_path
 
-    rows = len(input_data) * 2 - 1
-    cols = len(input_data[1]) * 2 - 1
-    large_grid = {Point(x, y) for y in range(rows) for x in range(cols)}
+    # remove the outside until there is no more outside
+    outside = {p for p in expanded if p.x in [0, W - 1] or p.y in [0, H - 1]}
+    while len(outside):
+        expanded -= outside
+        outside = {n for p in outside for n in p.cardinal_neighbors().values()}
+        outside &= expanded
 
-    path = [start]
-    grid_step = start * 2
-    grid_path = {grid_step, grid_step.neighbor(facing)}
-    while step not in path:
-        path.append(step)
-        facing = TURN[board[step]][facing]
+    # contract the board by a factor of 2
+    contracted = {Point(x, y) for y in range(0, H, 2) for x in range(0, W, 2)}
+    trapped = expanded & contracted
 
-        grid_step = step * 2
-        grid_path |= {grid_step, grid_step.neighbor(facing)}
-
-        step = step.neighbor(facing)
-
-    new_outside = set(
-        Point(x, y)
-        for y in range(rows)
-        for x in range(cols)
-        if x == 0 or y == 0 or x == cols - 1 or y == rows - 1
-    )
-    new_outside -= grid_path
-    outside = set()
-
-    while len(new_outside):
-        outside |= new_outside
-        new_outside = set().union(
-            n
-            for point in new_outside
-            for n in point.cardinal_neighbors().values()
-            if n in large_grid
-        )
-        new_outside -= outside
-        new_outside -= grid_path
-
-    large_grid -= outside
-    large_grid -= grid_path
-
-    small_grid = {Point(x, y) for y in range(0, rows, 2) for x in range(0, cols, 2)}
-
-    return len(small_grid & large_grid)
+    return len(trapped)
