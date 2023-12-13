0b896049db16ddace4405220c7ea4124ecae1c38
Alex Mabry <alex.mabry@adtran.com>
2023-12-13 10:18:55 -0600

    fun: day 13 part 1

--- aoc23/src/puzzles/day13.py
+++ aoc23/src/puzzles/day13.py
@@ -1,11 +1,36 @@
+from itertools import pairwise
+
 from ..utils import parse_data
 
 
-def solve_part1(data: str):
-    input_data = parse_data(data)
-    print(input_data)
+def find_fold(chars: [str]):
+    return next(
+        (
+            p + 1
+            for p, (i, j) in enumerate(pairwise(chars))
+            if i == j
+            and all(
+                chars[p - i] == chars[p + i + 1]
+                for i in range(p + 1)
+                if len(chars) - i - 1 > p >= i
+            )
+        ),
+        0,
+    )
 
-    return None
+
+def solve_part1(data: str):
+    by_row = []
+
+    input_data = parse_data(data)
+    while input_data:
+        by_row.append([])
+        while input_data and (row := input_data.pop(0)):
+            by_row[-1].append(row)
+
+    by_col = [["".join(row[i] for row in p) for i in range(len(p[0]))] for p in by_row]
+
+    return sum(find_fold(r) * 100 + find_fold(c) for r, c in zip(by_row, by_col))
 
 
 def solve_part2(data: str):
