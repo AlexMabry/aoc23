f42134fd04328d5ad4471c0462953e3f9978e23c
Alex Mabry <alex.mabry@adtran.com>
2023-12-14 15:54:31 -0600

    fun: day 14 part 1

--- aoc23/src/puzzles/day14.py
+++ aoc23/src/puzzles/day14.py
@@ -1,11 +1,34 @@
+from itertools import batched, pairwise
+from time import sleep
+
 from ..utils import parse_data
+
+
+def land(ca, cb):
+    return "*" if ca == "." and cb == "O" else ca
+
+
+def fall(ca, cb):
+    return "." if ca == "*" else cb
+
+
+def count(rocks, target="O"):
+    return sum([1 for c in rocks if c == target])
 
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    rocks = [[c for c in row] for row in input_data]
 
-    return None
+    before = []
+    while before != rocks:
+        before = rocks.copy()
+        for a, b in pairwise(range(len(rocks))):
+            rocks[a] = [land(ca, cb) for ca, cb in zip(rocks[a], rocks[b])]
+            rocks[b] = [fall(ca, cb) for ca, cb in zip(rocks[a], rocks[b])]
+            rocks[a] = ["O" if ca == "*" else ca for ca in rocks[a]]
+
+    return sum((len(rocks) - i) * count(row) for i, row in enumerate(rocks))
 
 
 def solve_part2(data: str):
