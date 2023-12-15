12fd2edeeb417ca5b9b3a04b69f2e3a6aef734bb
Alex Mabry <alex.mabry@adtran.com>
2023-12-15 09:57:12 -0600

    fun: day 15 part 2

--- aoc23/src/puzzles/day15.py
+++ aoc23/src/puzzles/day15.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, OrderedDict
 from functools import reduce
 from itertools import chain
 
@@ -11,12 +12,24 @@ def hash_val(string):
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    answer = sum(hash_val(step) for step in "".join(input_data).split(","))
-    return answer
+    return sum(hash_val(step) for step in "".join(input_data).split(","))
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
-    print(input_data)
+    steps = "".join(input_data).replace("-", "=").split(",")
+    commands = ([c for c in s.split("=") if c] for s in steps)
 
-    return None
+    boxes = defaultdict(OrderedDict)
+    for cmd in commands:
+        label = hash_val(cmd[0])
+        if len(cmd) == 2:
+            boxes[label][cmd[0]] = int(cmd[1])
+        elif cmd[0] in boxes[label]:
+            del boxes[label][cmd[0]]
+
+    return sum(
+        (ib + 1) * (il + 1) * focal
+        for ib, box in boxes.items()
+        for il, focal in enumerate(box.values())
+    )
