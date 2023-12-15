ed480d4743724468b6d8e7c930c328890898bd59
Alex Mabry <alex.mabry@adtran.com>
2023-12-15 09:09:06 -0600

    fun: day 15 part 1

--- aoc23/src/puzzles/day15.py
+++ aoc23/src/puzzles/day15.py
@@ -3,9 +3,16 @@ from ..utils import parse_data
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-    print(input_data)
 
-    return None
+    answer = 0
+    for step in "".join(input_data).split(","):
+        current_value = 0
+        for c in step:
+            current_value = ((current_value + ord(c)) * 17) % 256
+
+        answer += current_value
+
+    return answer
 
 
 def solve_part2(data: str):
