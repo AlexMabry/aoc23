cbd20ae449751c980eb74b8fd47fef5d3250a693
Alex Mabry <alexmabry@gmail.com>
2023-12-14 22:08:39 -0600

    fun: day 14 part 2

--- aoc23/src/puzzles/day14.py
+++ aoc23/src/puzzles/day14.py
@@ -1,19 +1,6 @@
-from itertools import batched, pairwise
-from time import sleep
+from itertools import pairwise
 
 from ..utils import parse_data
-
-
-def land(ca, cb):
-    return "*" if ca == "." and cb == "O" else ca
-
-
-def fall(ca, cb):
-    return "." if ca == "*" else cb
-
-
-def count(rocks, target="O"):
-    return sum([1 for c in rocks if c == target])
 
 
 def tilt(rocks):
@@ -21,62 +8,55 @@ def tilt(rocks):
     while before != rocks:
         before = rocks.copy()
         for a, b in pairwise(range(len(rocks))):
-            rocks[a] = [land(ca, cb) for ca, cb in zip(rocks[a], rocks[b])]
-            rocks[b] = [fall(ca, cb) for ca, cb in zip(rocks[a], rocks[b])]
+            rocks[a] = [
+                "*" if (ca, cb) == (".", "O") else ca
+                for ca, cb in zip(rocks[a], rocks[b])
+            ]
+            rocks[b] = ["." if ca == "*" else cb for ca, cb in zip(rocks[a], rocks[b])]
             rocks[a] = ["O" if ca == "*" else ca for ca in rocks[a]]
     return rocks
+
+
+def weigh(rocks):
+    return sum(
+        (len(rocks) - i) * sum([1 for c in row if c == "O"])
+        for i, row in enumerate(rocks)
+    )
+
+
+def diff(before, after):
+    return tuple(
+        (x, y)
+        for y, (a, b) in enumerate(zip(before, after))
+        for x, (ca, cb) in enumerate(zip(a, b))
+        if ca != cb
+    )
 
 
 def solve_part1(data: str):
     input_data = parse_data(data)
     rocks = [[c for c in row] for row in input_data]
-
     rocks = tilt(rocks)
-
-    return sum((len(rocks) - i) * count(row) for i, row in enumerate(rocks))
-
-
-def rotate(rocks):
-    return list(zip(*reversed(rocks)))
-
-
-def condense(rocks):
-    return "".join("".join(row) for row in rocks)
-
-
-def hydrate(rocks):
-    return [[c for c in row] for row in rocks]
+    return weigh(rocks)
 
 
 def solve_part2(data: str):
     input_data = parse_data(data)
     rocks = [[c for c in row] for row in input_data]
 
-    l1_cache = {}
-    l2_cache = {}
+    remaining = 1_000_000_000
+    pattern = []
+    while (remaining := remaining - 1) > 0:
+        before = rocks.copy()
+        for _ in range(4):
+            rocks = list(zip(*reversed(tilt(rocks))))
 
-    l2_after = rocks.copy()
-    l1_key = l2_key = None
-    for _ in range(1_000_000_000):
-        if _ % 1_000_000 == 0:
-            print(f"  {_}")
+        if 10_000 < remaining <= 999_999_900:
+            if not pattern:
+                pattern = [diff(before, rocks)]
+            elif (changed := diff(before, rocks)) != pattern[0]:
+                pattern.append(changed)
+            else:
+                remaining = remaining % len(pattern)
 
-        l2_before = l2_after
-        if l2_key in l2_cache:
-            l2_after = hydrate((l2_key := l2_cache[l2_key]))
-        else:
-            l1_after = l2_before
-            for __ in range(4):
-                l1_before = l1_after
-                if l1_key in l1_cache:
-                    l1_after = hydrate((l1_key := l1_cache[l1_key]))
-                else:
-                    l1_after = rotate(tilt(l1_before))
-                    l1_cache[l1_key] = (l1_key := condense(l1_after))
-
-            l2_after = l1_after
-            l2_cache[l2_key] = (l2_key := condense(l2_after))
-
-    print("\n".join("".join(row) for row in rocks))
-
-    return None
+    return weigh(rocks)
