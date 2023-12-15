2fd2b0c83c4c90408b7d41b97bdf3f4b4cad8a3c
Alex Mabry <alex.mabry@adtran.com>
2023-12-15 09:20:18 -0600

    fun: day 15 part 1 clean

--- aoc23/src/puzzles/day15.py
+++ aoc23/src/puzzles/day15.py
@@ -1,17 +1,17 @@
+from functools import reduce
+from itertools import chain
+
 from ..utils import parse_data
+
+
+def hash_val(string):
+    ords = chain([0], (ord(c) for c in string))
+    return reduce(lambda a, b: (a + b) * 17 % 256, ords)
 
 
 def solve_part1(data: str):
     input_data = parse_data(data)
-
-    answer = 0
-    for step in "".join(input_data).split(","):
-        current_value = 0
-        for c in step:
-            current_value = ((current_value + ord(c)) * 17) % 256
-
-        answer += current_value
-
+    answer = sum(hash_val(step) for step in "".join(input_data).split(","))
     return answer
 
 
