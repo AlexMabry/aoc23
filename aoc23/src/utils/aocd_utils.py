5dad37a7f562f37aacbef3712e503650b0f2c70a
Alex Mabry <alexmabry@gmail.com>
2023-12-24 18:11:54 -0600

    fun: day 24 part 1

--- aoc23/src/utils/aocd_utils.py
+++ aoc23/src/utils/aocd_utils.py
@@ -2,14 +2,23 @@ import re
 from types import SimpleNamespace
 
 
-def parse_data(input_data, *, is_lines=True, is_numbers=False, regex=None):
+def parse_data(
+    input_data, *, is_lines=True, is_numbers=False, regex=None, findall=None
+):
     lines = input_data.splitlines()
     if regex:
         pattern = re.compile(regex)
         if pattern.groupindex.keys():
-            return [SimpleNamespace(**pattern.search(item).groupdict()) for item in lines if item]
+            return [
+                SimpleNamespace(**pattern.search(item).groupdict())
+                for item in lines
+                if item
+            ]
         else:
             return [pattern.search(item).groups() for item in lines if item]
+    elif findall:
+        pattern = re.compile(findall)
+        return [re.findall(pattern, item) for item in lines if item]
 
     if is_numbers:
         return [int(n) for n in lines]
