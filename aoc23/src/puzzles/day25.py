1da11453210e7a9abac8a189a5b2edcfb85be574
Alex Mabry <alexmabry@gmail.com>
2023-12-30 16:17:24 -0600

    fun: day 25 part 1

--- aoc23/src/puzzles/day25.py
+++ aoc23/src/puzzles/day25.py
@@ -1,11 +1,19 @@
+from functools import reduce
+
+import networkx as nx
+
 from ..utils import parse_data
 
 
 def solve_part1(data: str):
-    input_data = parse_data(data)
-    print(input_data)
+    input_data = parse_data(data, regex=r"(\w+): (.*)")
+    edges = {n: set(e.split()) for n, e in input_data}
 
-    return None
+    G = nx.Graph(edges)
+    G.remove_edges_from(nx.minimum_edge_cut(G))
+
+    sizes = [len(component) for component in nx.connected_components(G)]
+    return reduce(lambda x, y: x * y, sizes)
 
 
 def solve_part2(data: str):
