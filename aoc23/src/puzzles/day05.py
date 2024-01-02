e6d43d3591a502b8ea16d40ef03f0f6b7c23cc6f
Alex Mabry <alexmabry@gmail.com>
2024-01-02 10:05:40 -0600

    fun: day 5 part 2

--- aoc23/src/puzzles/day05.py
+++ aoc23/src/puzzles/day05.py
@@ -1,22 +1,29 @@
 import re
-from itertools import pairwise
+from itertools import pairwise, batched
 from typing import NamedTuple
 
 from ..utils import parse_data
 
 
-class Transform(NamedTuple):
+class Range(NamedTuple):
     start: int
     end: int
+
+    def __contains__(self, value):
+        return self.start <= value < self.end
+
+
+class Transform(NamedTuple):
+    range: Range
     offset: int
 
     @classmethod
     def from_row(cls, row):
         dst, src, rlen = map(int, row.split())
-        return Transform(src, src + rlen - 1, dst - src)
+        return Transform(Range(src, src + rlen), dst - src)
 
-    def __contains__(self, value):
-        return self.start <= value <= self.end
+    def __contains__(self, r):
+        return r.start in self.range or r.end - 1 in self.range
 
 
 def parse_map_xforms(input_data):
@@ -35,7 +42,7 @@ def solve_part1(data: str):
     lowest = float("inf")
     for seed in seeds:
         for xforms in map_xforms:
-            seed = next((seed + t.offset for t in xforms if seed in t), seed)
+            seed = next((seed + t.offset for t in xforms if seed in t.range), seed)
         lowest = min(lowest, seed)
 
     return lowest
@@ -43,5 +50,27 @@ def solve_part1(data: str):
 
 def solve_part2(data: str):
     input_data = parse_data(data)
+    seeds_match = re.match(r"seeds:\s+((\d+\s?)+)", input_data[0])
+    seeds = map(int, seeds_match.group(1).split())
+    map_xforms = parse_map_xforms(input_data)
+    ranges = [Range(a, a + b) for a, b in batched(seeds, 2)]
+    for xforms in map_xforms:
+        result = []
+        while ranges and (r := ranges.pop(0)):
+            if match := next((t for t in xforms if r in t), None):
+                result.append(
+                    Range(
+                        max(r.start, match.range.start) + match.offset,
+                        min(r.end, match.range.end) + match.offset,
+                    )
+                )
+                if r.start < match.range.start:
+                    ranges.append(Range(r.start, match.range.start))
+                if r.end > match.range.end:
+                    ranges.append(Range(match.range.end, r.end))
+            else:
+                result.append(r)
 
-    return None
+        ranges = result
+
+    return min(r.start for r in ranges)
